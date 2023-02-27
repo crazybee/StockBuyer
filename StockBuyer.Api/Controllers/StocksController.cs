d97143ca091582dc80ac348ff4e21980d6bcfbb8
crazybeeliuzhe <crazybee@users.noreply.github.com>
2023-02-27 18:01:45 +0100

    wip

--- StockBuyer.Api/Controllers/StocksController.cs
+++ StockBuyer.Api/Controllers/StocksController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StockBuyer.Api.Attributes;
+using StockBuyer.Contracts.DTOs;
 using StockBuyer.Data.Services;
 
 namespace StockBuyer.Api.Controllers
@@ -9,21 +10,21 @@ namespace StockBuyer.Api.Controllers
     [ApiController]
     public class StocksController : BaseController
     {
-        protected StocksController(IUserService userService) : base(userService)
+        private readonly IStocksDataService _stocksDataService;
+
+
+        protected StocksController(IUserService userService, IStocksDataService stocksDataService) : base(userService)
         {
+            this._stocksDataService = stocksDataService;
+        }
+
+        
             [CrazybeeAuthorize]
             [HttpGet("allstocks")]
             public async Task<ActionResult<List<StockDto>>?> GetAllStocks()
             {
-
-                var validUser = await IsUserValidInContext(doorRequest.UserName);
-                if (validUser == null || validUser.UserType != UserType.Administrator)
-                {
-                    return this.BadRequest();
-                }
-
-                return await this.doorHistoryService.GetDoorHistoryItemsAsync(doorRequest.DoorId);
+                return await this._stocksDataService.GetAllStocks();
             }
-        }
+        
     }
 }
